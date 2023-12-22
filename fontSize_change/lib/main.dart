958cdcf43bdc227734a480867fa617a8823e0961
Vanshdarji <134920483+Vanshdarji@users.noreply.github.com>
2023-12-22 16:01:14 +0530

    Add files via upload

---
+++ fontSize_change/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ui_deisgn/fontSize_change/lib/fontSize_change.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Apps',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const FontSizeChange(),
+    );
+  }
+}
+
