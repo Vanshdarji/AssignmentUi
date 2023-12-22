e6f5328c4b5cf681e348a5de43d29120c6a98d67
Vanshdarji <134920483+Vanshdarji@users.noreply.github.com>
2023-12-22 16:02:06 +0530

    Add files via upload

---
+++ images_around_text/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ui_deisgn/images_around_text/lib/images_around_text.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Apps',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyImages(),
+    );
+  }
+}
+
